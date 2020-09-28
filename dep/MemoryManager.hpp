b7dad175422cd48e1f6a4137010e34a060cdc2fb
Nikita Phung <nikitaphung2000@gmail.com>
2020-09-28 11:31:33 +1000

    Updated MemoryManager.cpp

--- dep/MemoryManager.hpp
+++ dep/MemoryManager.hpp
@@ -10,11 +10,29 @@ class MemoryManager {
     public:
         MemoryManager();
         ~MemoryManager();
-        virtual void allocateMemory();
+        
+        
         list<MemoryBlock*> test;
 
     protected:
+        // Local variable for the data
         list<char*> data;
+
+        // Merge consecutive blocks if they are free in freeMBList
+        void mergeBlocks();
+
+        // Move memory blocks form allocMBList to freeMBList
+        void freeMemory(int blocks);
+
+        // For allocating memory
+        virtual void allocateMemory(int blocks);
+
+        /* Runs the Memory Manager according to how 
+        many blocks are allocated and deleted while 
+        there is still unallocated data */
+        virtual void run(int allocate, int free);
+
+        
         list<MemoryBlock*> allocMBList;
         list<MemoryBlock*> freedMBList;
 
---
+++ src/MemoryManager.cpp
@@ -0,0 +1,18 @@
+#include "../dep/MemoryManager.hpp"
+
+MemoryManager::MemoryManager() {}
+
+MemoryManager::~MemoryManager() {}
+
+void MemoryManager::allocateMemory() {
+    // Each Memory Manager will have its own implementation of allocateMemory().
+}
+
+void MemoryManager::freeMemory() {
+
+}
+
+void MemoryManager::mergeBlocks() {
+
+}
+
