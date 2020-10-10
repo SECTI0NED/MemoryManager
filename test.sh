827afcf4c1fc7a891f5196bd0ad86bbeae1e0df6
Nikita Phung <nikitaphung2000@gmail.com>
2020-10-10 19:30:47 +1100

    Updated test file

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+# This was used for testing and debugging
+
 # ./program _data/test/test1.txt ff
 # ./program _data/test/test1.txt bf
 # ./program _data/test/test1.txt wf
@@ -6,7 +8,6 @@
 # ./program _data/test/test2.txt bf
 # ./program _data/test/test2.txt wf
 
-# ./program _data/test/test3.txt ff
-# ./program _data/test/test3.txt bf 
-# ./program _data/test/test3.txt wf
-
+./program _data/test/test3.txt ff
+./program _data/test/test3.txt bf 
+./program _data/test/test3.txt wf
