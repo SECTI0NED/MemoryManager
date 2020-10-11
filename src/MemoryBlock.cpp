f726d65f8d131b92f085e285172ccab9fda3d070
Nikita Phung <nikitaphung2000@gmail.com>
2020-10-11 14:32:01 +1100

    Updated resetData

--- src/MemoryBlock.cpp
+++ src/MemoryBlock.cpp
@@ -16,14 +16,23 @@ void MemoryBlock::setData(char* data) {
     this->data = data;
 }
 
-/* Had to resort to using sbrk here, changing 
-the actual data resulted in segfaults */
+/* Had to resort to changing the this->data to point to a different 
+memory location or using sbrk to get a new chunk of memory, because changing 
+the actual data resulted in segmentation faults.
+This is because this->data is type 'char*' which is unmodifable.
+For this->data be be overwritten, we have to change the type to 'char[SIZE]', 
+but since we actually do not know the sizeupfront this is impossible to implement.
+Sources:    
+https://cboard.cprogramming.com/c-programming/153924-segmentation-fault-when-replacing-char-array.html
+https://stackoverflow.com/questions/37164358/error-flexible-array-member-not-at-end-of-struct */
 void MemoryBlock::resetData(const char* data){
-    void* request = sbrk(size);
-    strcpy((char*) request, data);
-    this->data = (char*) request;
-    this->startingAddress = (char**) request;
 
+    this->data = new char[size];
+    strcpy(this->data, data);
+    this->startingAddress = (char**) this->data;
+
+    /* This is will cause segfaults: */
+    // this->data = newData;
     // int counter = 0;
     // while(counter < size) {
     //     this->data[counter] = data[counter];
